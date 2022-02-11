db766de6ffc6894c7906b269017beae2e159a227
Dmitry Kharitonov <dimitrianderi@gmail.com>
2022-02-11 23:41:49 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const length = stack.lenght;
+
+  str = str.split('').map(el => {
+
+    isOpen(el);
+
+    function isOpen(item) {
+      for (let el of bracketsConfig) {
+        if (el[0] === item && (el[0] !== el[1] || (el[0] === el[1] && (stack[stack.length - 1] !== item || stack.length === 0)))) {
+          stack.push(item);
+          return;
+        }
+      }
+      isCompare(item);
+    }
+
+    function isCompare(item) {
+      let ind = 0;
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        if ((stack[stack.length - 1]) === bracketsConfig[i][0]) ind = i;
+      }
+      if ((stack.length !== 0) && (item === bracketsConfig[ind][1])) {
+        stack.pop();
+      } else {
+        stack.push(item)
+      }
+    }
+
+    
+  });
+
+  return (stack.length === 0) ? true : false;
+
 }
